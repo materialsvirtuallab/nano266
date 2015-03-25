f4ba3945349c4b52de2508517ce1855de462fb6d
__sp__ <ongsp@eng.ucsd.edu>
2015-03-24 20:13:03 -0700

    Tabs to spaces.

--- labs/lab2/run_pw.py
+++ labs/lab2/run_pw.py
@@ -16,7 +16,7 @@ import numpy as np
 
 # Load the Fe.pw.in.template file as a template.
 with open("Si.pw.in.template") as f:
-	template = f.read()
+    template = f.read()
 
 # Set the k-point grid
 k = 8
@@ -28,18 +28,18 @@ alat = 10 # The lattice parameter for the cell in Bohr.
 # other python functions that are alien to you). When writing code to automate
 # anything, you frequently need to consult documentation on the web. 
 for ecut in np.arange(10, 50, 10):
-	s = template.format(alat=alat, k=k, ecut=ecut)
-	jobname = "Si_%s" % ecut
-	
-	# Write the actual input file for PWSCF.
-	with open("%s.pw.in" % jobname, "w") as f:
-		f.write(s)
+    s = template.format(alat=alat, k=k, ecut=ecut)
+    jobname = "Si_%s" % ecut
+    
+    # Write the actual input file for PWSCF.
+    with open("%s.pw.in" % jobname, "w") as f:
+        f.write(s)
 
-	#Print some status messages.
-	print("Running with ecut = %f, alat = %f, k = %f..." % (ecut, alat, k))
-	# Run PWSCF. Modify the pw.x command accordingly if needed.
-	os.system("pw.x.mac < {jobname}.pw.in > {jobname}.out".format(jobname=jobname))
+    #Print some status messages.
+    print("Running with ecut = %f, alat = %f, k = %f..." % (ecut, alat, k))
+    # Run PWSCF. Modify the pw.x command accordingly if needed.
+    os.system("pw.x.mac < {jobname}.pw.in > {jobname}.out".format(jobname=jobname))
 
-	print("Done")
+    print("Done")
 
 shutil.rmtree("tmp")
