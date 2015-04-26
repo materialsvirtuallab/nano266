6b86f2c91b58af81d01e1aa16cf2ca765d2eb228
adengz <adengz@gmail.com>
2015-04-26 14:30:20 -0700

    fix total_force missing

--- labs/lab2/analyze.py
+++ labs/lab2/analyze.py
@@ -30,7 +30,7 @@ def get_results(filename):
 
 
 def analyze(filenames):
-    fieldnames = ['filename', 'ecut', 'nkpts', 'alat', 'energy']
+    fieldnames = ['filename', 'ecut', 'nkpts', 'alat', 'energy','total_force']
     with open('results.csv', 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
