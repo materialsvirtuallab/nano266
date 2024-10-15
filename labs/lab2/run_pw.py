97b253c370ab2f8328705e56568e5764ca89ce67
Shyue Ping Ong <shyuep@gmail.com>
2024-10-15 08:00:26 -0700

    Update run_pw.py to modernize it.

--- labs/lab2/run_pw.py
+++ labs/lab2/run_pw.py
@@ -34,18 +34,17 @@ for ecut in [10, 20, 30, 40, 50]:
     s = template.format(alat=alat, k=k, ecut=ecut, pseudopotential=psp)
 
     # Let's define an easy jobname.
-    jobname = "Si_%s_%s_%s" % (ecut, k, alat)
+    jobname = f"Si_{ecut}_{k}_{alat}"
 
     # Write the actual input file for PWSCF.
-    with open("%s.pw.in" % jobname, "w") as f:
+    with open(f"{jobname}.pw.in", "w") as f:
         f.write(s)
 
     # Write the command in submit_script.    
     submit_script.write(
             'mpirun --map-by core --mca btl_openib_if_include "mlx5_2:1" ' 
-            '--mca btl openib,self,vader pw.x -input {jobname}.pw.in -npool 1 > {jobname}.out\n'
-            .format(jobname=jobname))
-    print("Done with input generation for %s" % jobname)
+            f'--mca btl openib,self,vader pw.x -input {jobname}.pw.in -npool 1 > {jobname}.out\n')
+    print(f"Done with input generation for {jobname}.")
 
 # Append another line in submit_script to cleanup. 
 # For this lab, we don't need the files that are dumped into the tmp directory.
